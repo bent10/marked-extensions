b18767cc5f6fdbd1bc385cb5afdd6b1f8ee06fe7
bent10 <bent10@stilearning.com>
2023-10-19 16:00:23 +0700

    build: tweaks

--- packages/code-jsx-renderer/vite.config.ts
+++ packages/code-jsx-renderer/vite.config.ts
@@ -4,6 +4,9 @@ import cacheDir from 'vite-plugin-cachedir'
 
 export default defineConfig({
   plugins: [cacheDir()],
+  build: {
+    ssr: 'src/index.ts'
+  },
   test: {
     globals: true,
     include: ['test/*.test.ts']
