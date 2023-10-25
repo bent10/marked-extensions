c961c429d3b3da5f83181dd660f185ca68fa1428
bent10 <bent10@stilearning.com>
2023-10-25 20:16:35 +0700

    fix: resolves mime issue

--- packages/directive/vite.config.ts
+++ packages/directive/vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
         globals: {
           'attribute-parser': 'parseAttrs',
           moo: 'moo'
-        }
+        },
+        entryFileNames: '[name].[format].js'
       }
     }
   },
