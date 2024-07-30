2077d588cf1229c2c5348ea3e958e6f64e8cbe9b
irian <irian.code@hotmail.com>
2024-07-30 17:20:57 +0200

    feat: :arrow_up: Ported marked-plaintify to Marked v13.
    Updated the functions to use the new v13 Marked way, which includes breaking changes to the libraries that override the Renderer object (https://github.com/markedjs/marked/releases/tag/v13.0.0)
    The new functions all accept a 'token' parameter instead of individual parameters like before. But most importantly, they call the Parser instance to process nested tokens.
    Also added the new 'useNewRenderer' prop on the return object.

--- packages/plaintify/src/index.ts
+++ packages/plaintify/src/index.ts
@@ -8,7 +8,7 @@ export type Options = RendererObject & {
    * Custom 3rd-party renderers.
    */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [k: string]: (...args: any[]) => string | false
+  [k: string]: (this: Renderer, ...args: any[]) => string | false
 }
 
 /**
@@ -18,9 +18,9 @@ export default function markedPlaintify(
   options: Options = {}
 ): MarkedExtension {
   const plainTextRenderer: Options = {}
-  const mdIgnores: string[] = ['constructor', 'hr', 'checkbox', 'br']
-  const mdInlines: string[] = ['strong', 'em', 'codespan', 'del', 'text']
-  const mdEscapes: string[] = ['html', 'code', 'codespan']
+  const mdIgnores: string[] = ['constructor', 'hr', 'checkbox', 'br', 'space']
+  const mdInlines: string[] = ['strong', 'em', 'del']
+  const mdEscapes: string[] = ['html', 'code']
 
   let currentTableHeader: string[] = []
 
@@ -30,29 +30,70 @@ export default function markedPlaintify(
       plainTextRenderer[prop] = () => ''
     } else if (mdInlines.includes(prop)) {
       // preserve inline elements
-      plainTextRenderer[prop] = text => text
+      plainTextRenderer[prop] = function (token) {
+        const text = this.parser.parseInline(token.tokens)
+        return text
+      }
     } else if (mdEscapes.includes(prop)) {
       // escaped elements
-      plainTextRenderer[prop] = text => escapeHTML(text) + '\n\n'
+      plainTextRenderer[prop] = token => escapeHTML(token.text) + '\n\n'
+    } else if (prop === 'codespan') {
+      // handle codespan
+      plainTextRenderer[prop] = token => escapeHTML(token.text)
     } else if (prop === 'list') {
       // handle list element
-      plainTextRenderer[prop] = text => '\n' + text.trim() + '\n\n'
+      plainTextRenderer[prop] = function (token) {
+        let text = ''
+        for (let j = 0; j < token.items.length; j++) {
+          const item = token.items[j]
+          const renderedItem = this.listitem(item)
+
+          if (typeof renderedItem === 'string') {
+            text += renderedItem.replace(/\n{2,}/g, '\n')
+          }
+        }
+
+        return '\n' + text.trim() + '\n\n'
+      }
     } else if (prop === 'listitem') {
       // handle list items
-      plainTextRenderer[prop] = text => '\n' + text.trim()
+      plainTextRenderer[prop] = function (token) {
+        const text = this.parser.parse(token.tokens)
+        return '\n' + text.trim()
+      }
     } else if (prop === 'blockquote') {
-      // handle list items
-      plainTextRenderer[prop] = text => text.trim() + '\n\n'
+      // handle blockquote
+      plainTextRenderer[prop] = function (token) {
+        const text = this.parser.parse(token.tokens)
+        return text.trim() + '\n\n'
+      }
     } else if (prop === 'table') {
       // handle table elements
-      plainTextRenderer[prop] = (_, body) => {
+      plainTextRenderer[prop] = function (token) {
         currentTableHeader = []
+
+        // parsing headers
+        for (let j = 0; j < token.header.length; j++) {
+          this.tablecell(token.header[j])
+        }
+
+        // parsing rows
+        let body = ''
+        for (let j = 0; j < token.rows.length; j++) {
+          const row = token.rows[j]
+          let cell = ''
+          for (let k = 0; k < row.length; k++) {
+            cell += this.tablecell(row[k])
+          }
+          body += this.tablerow({ text: cell })
+        }
+
         return body
       }
     } else if (prop === 'tablerow') {
       // handle table rows
-      plainTextRenderer[prop] = content => {
-        const chunks = content.split('__CELL_PAD__').filter(Boolean)
+      plainTextRenderer[prop] = token => {
+        const chunks = token.text.split('__CELL_PAD__').filter(Boolean)
 
         return (
           currentTableHeader
@@ -62,22 +103,54 @@ export default function markedPlaintify(
       }
     } else if (prop === 'tablecell') {
       // handle table cells
-      plainTextRenderer[prop] = (text, flags) => {
-        if (flags.header) {
+      plainTextRenderer[prop] = function (token) {
+        const text = this.parser.parseInline(token.tokens)
+
+        if (token.header) {
           currentTableHeader.push(text)
         }
+
         return text + '__CELL_PAD__'
       }
-    } else if (prop === 'link' || prop === 'image') {
-      // handle links and images
-      plainTextRenderer[prop] = (_, __, text) => (!!text ? text : '')
+    } else if (prop === 'link') {
+      // handle links
+      plainTextRenderer[prop] = function (token) {
+        const text = this.parser.parseInline(token.tokens)
+        return text + '\n\n'
+      }
+    } else if (prop === 'image') {
+      // handle images (links starting with !)
+      plainTextRenderer[prop] = token => {
+        return token.text + '\n\n'
+      }
+    } else if (prop === 'paragraph') {
+      // handle paragraphs (which sometimes link and images lines are detected as well)
+      plainTextRenderer[prop] = function (token) {
+        let text = this.parser.parseInline(token.tokens)
+
+        // Removing extra newlines introduced by other renderer functions
+        text = text.replace(/\n{2,}/g, '')
+
+        return text + '\n\n'
+      }
+    } else if (prop === 'heading') {
+      // handle headings
+      plainTextRenderer[prop] = function (token) {
+        const text = this.parser.parseInline(token.tokens)
+        return text + '\n\n'
+      }
     } else {
-      // handle other (often block-level) elements
-      plainTextRenderer[prop] = text => text + '\n\n'
+      // handle other elements
+      plainTextRenderer[prop] = function (token) {
+        return 'tokens' in token && token.tokens
+          ? this.parser.parseInline(token.tokens)
+          : token.text
+      }
     }
   })
 
   return {
+    useNewRenderer: true,
     renderer: {
       ...plainTextRenderer,
       ...options
