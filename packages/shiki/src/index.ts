ecd9c074ec5adce1d3112440c4a5ebbfa8e15559
rekram1-node <aidenpcline@gmail.com>
2025-08-04 09:24:18 -0500

    fix: cannot read properties of undefined (reading 'split') #180

--- packages/shiki/src/index.ts
+++ packages/shiki/src/index.ts
@@ -38,7 +38,7 @@ export default function markedShiki(options: Options = {}): MarkedExtension {
     async walkTokens(token) {
       if (token.type !== 'code' || typeof highlight !== 'function') return
 
-      const [lang = 'text', ...props] = token.lang.split(' ')
+      const [lang = 'text', ...props] = token.lang?.split(' ') ?? []
 
       const { text } = token
       const highlightedText = await highlight(text, lang, props)
